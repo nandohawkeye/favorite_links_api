a72357ee62c12edff5de9fff02110ea9b26050cc
nandohawkeye <luizfernando.m.neves@gmail.com>
2026-04-03 08:58:23 -0300

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
 import swaggerUi from 'swagger-ui-express';
 import { swaggerSpec } from './swagger';
 import authRoutes from './routes/auth.routes';
@@ -8,6 +9,14 @@ import tagRoutes from './routes/tag.routes';
 import { errorHandler } from './middlewares/erroHandler';
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGIN,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
+);
 
 app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
